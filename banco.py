78b61fdbd2659e4f66ec43769b8c5cde35d3a1fb
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-04-26 19:04:18 -0300

    CRUD em Python com SQLite

---
+++ banco.py
@@ -0,0 +1,29 @@
+# import SQLite
+import sqlite3 as lite
+
+# Criando conexão
+con = lite.connect('dados.db')
+
+# Criando tabela
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                "nome TEXT, "
+                "telefone TEXT,  "
+                "dia_em DATE, "
+                "estado TEXT, assunto TEXT)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,96 @@
+from cgitb import text
+from tkinter import *
+from tkinter import font
+
+# Importando caledario
+from tkcalendar import Calendar, DateEntry
+
+# Cores
+co0 = "#f0f3f5"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"   # letra
+co5 = "#e06636"   # - profit
+co6 = "#038cfc"   # azul
+co7 = "#ef5350"   # vermelha
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # sky blue
+
+# Criando janela
+
+janela = Tk()
+janela.title("")
+janela.geometry('1043x453')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
+
+# Dividindo janela
+frame_cima = Frame(janela, width=310, height=50, bg=co2, relief='flat')
+frame_cima.grid(row=0, column=0)
+
+frame_baixo = Frame(janela, width=310, height=403, bg=co1, relief='flat')
+frame_baixo.grid(row=1, column=0, sticky=NSEW, padx=0, pady=1)
+
+frame_direita = Frame(janela, width=588, height=403, bg=co1, relief='flat')
+frame_direita.grid(row=0, column=1, rowspan=2, padx=1, sticky=NSEW)
+
+# Label cima
+app_nome = Label(frame_cima, text='Formulário de Consultoria', anchor=NW, font=('Ivy 13 bold'), bg=co2, fg=co1, relief='flat')
+app_nome.place(x=15, y=20)
+
+# Configurando Frame baixo
+# Nome
+l_nome = Label(frame_baixo, text='Nome *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+l_nome.place(x=15, y=10)
+e_nome = Entry(frame_baixo, width=45, justify='left', relief='solid')
+e_nome.place(x=15, y=40)
+
+# Email
+l_email = Label(frame_baixo, text='Email *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+l_email.place(x=15, y=70)
+e_email = Entry(frame_baixo, width=45, justify='left', relief='solid')
+e_email.place(x=15, y=100)
+
+# Telefone
+l_telefone = Label(frame_baixo, text='Telefone *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+l_telefone.place(x=15, y=130)
+e_telefone = Entry(frame_baixo, width=45, justify='left', relief='solid')
+e_telefone.place(x=15, y=160)
+
+# Data da Consulta
+l_cal = Label(frame_baixo, text='Data da Consulta *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+l_cal.place(x=15, y=190)
+e_cal = DateEntry(frame_baixo, width=12, background='darkblue', foreground='white', borderwidth='2', year=2023)
+e_cal.place(x=15, y=220)
+
+# Estado
+l_estado = Label(frame_baixo, text='Estado da Consulta *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+l_estado.place(x=160, y=190)
+e_estado = Entry(frame_baixo, width=20, justify='left', relief='solid')
+e_estado.place(x=160, y=220)
+
+# Sobre
+l_sobre = Label(frame_baixo, text='Informação extra', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+l_sobre.place(x=15, y=260)
+e_sobre = Entry(frame_baixo, width=45, justify='left', relief='solid')
+e_sobre.place(x=15, y=290)
+
+# Botão inserir
+b_inserir = Button(frame_baixo, text='Inserir', width=10, font=('Ivy 9 bold'), bg=co6, fg=co1, relief='raised', overrelief='ridge')
+b_inserir.place(x=15, y=340)
+
+# Botão atualizar
+b_inserir = Button(frame_baixo, text='Atualizar', width=10, font=('Ivy 9 bold'), bg=co2, fg=co1, relief='raised', overrelief='ridge')
+b_inserir.place(x=110, y=340)
+
+# Botão deletar
+b_inserir = Button(frame_baixo, text='Deletar', width=10, font=('Ivy 9 bold'), bg=co7, fg=co1, relief='raised', overrelief='ridge')
+b_inserir.place(x=205, y=340)
+
+
+
+
+
+
+janela.mainloop()
